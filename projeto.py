4f2695c9f2fcb8f308def135fe9485b9e34d7049
Matias Amma <matiasamma@gmail.com>
2024-05-24 17:18:46 -0300

    Falta função de identificar caso o valor inserido pelo usuario na hora de apagar um produto nao exista na tabela

--- projeto.py
+++ projeto.py
@@ -1,22 +1,70 @@
+import mysql.connector
+# SQL Connector é uma biblioteca usada para conectar o SQL ao Python.
 # Ao definiar uma função, facilitamos o processo de escrever repetidamente os comandos. Uma função pode ter vários comandos;
 # Neste caso, a função linhas() facilita a inserção de linhas de separação, ajudando no entendimento das informações no console.
 def linhas():
-    print('-'*130)
+    print('-'*190)
+
+def obtemConexaoComMySQL(servidor, usuario, senha, bd):
+    try:
+        conexao = mysql.connector.connect(
+            host=servidor,
+            user=usuario,
+            passwd=senha,
+            database=bd
+        )
+        linhas()
+        print("Conexão ao banco de dados MySQL bem-sucedida!")
+        linhas()
+        return conexao
+    except mysql.connector.Error as e:
+        print("Erro ao conectar ao banco de dados MySQL:", e)
+        return None
+
+# Função para executar comandos SQL
+def comandoSQL(comando, select=False):
+    conexao = obtemConexaoComMySQL("127.0.0.1", "root", "rootroot", "bd080324190")
+    if conexao:
+        try:
+            cursor = conexao.cursor()
+            cursor.execute(comando)
+            
+            if select:
+                dadosSelecionados = cursor.fetchall()
+                if dadosSelecionados:
+                    colunas = [i[0] for i in cursor.description] # Acessamos a propriedade description do cursor, que é uma sequência de tuplas. Cada tupla representa uma coluna na tabela.
+                    # Definindo larguras das colunas
+                    larguras = [max(len(str(dado)) for dado in coluna) for coluna in zip(colunas, *dadosSelecionados)] # Calculamos a largura necessária da coluna a partir da quantidade de caracteres no nome da Coluna. Essas larguras são armazenadas na lista larguras.
+                    # Imprimindo cabeçalho da tabela
+                    print(" | ".join("{:{}}".format(coluna, largura) for coluna, largura in zip(colunas, larguras))) # Utilizamos "{:{}}" na string de formatação para garantir que cada coluna seja impressa com a largura apropriada.
+                    # Imprimindo linha de separação
+                    linhas()
+                    # Imprimindo os dados
+                    for linha in dadosSelecionados:
+                        print(" | ".join("{:{}}".format(dado, largura) for dado, largura in zip(linha, larguras))) # A cada linha de informação é printado o dado com o espaçamento necessário.
+                else:
+                    print("Nenhum dado encontrado na tabela.")
+            else:
+                conexao.commit()
+                linhas()
+                print("Operação bem-sucedida!")
+                
+        except mysql.connector.Error as e:
+            print("Erro ao executar comando SQL:", e)
+            
+        finally:
+            if conexao.is_connected():
+                cursor.close()
+                conexao.close()
+                linhas()
+                print("Conexão ao banco de dados MySQL fechada.")
 
 # A função inserirDados() pede ao usuário os dados que serão inseridos, sendo acessível através do menu.
 def inserirDados():
-    global tabela_valores
-
-    # O loop 'while' recebe uma condição, neste caso a condição booleana True;
-    # Enquanto a condição for mantida, o loop continuará; Caso a condição seja alterada, o loop será interrompido.
-    # A estrutura de controle 'if' recebe condições; 
-    # A estrutura 'and' delimita que ambas as condições tenham que ser atendidas para seguir os comandos.
-    # A estrutura 'else' delimita que em condições adversas às mencionadas, certos comandos sejam acionados.
-
     while True:
         try:
             linhas()
-            primary_key = int(input('Insira o código do produto: '))                            #PK
+            primary_key = int(input('Insira o código do produto: '))
             break
         except ValueError:
             linhas()
@@ -24,10 +72,10 @@ def inserirDados():
 
     while True:
         linhas()
-        product_name = input('Insira o nome do produto: ')                                      #PN
-        product_description = input('Insira a descrição do produto: ')                          #PD
-        if product_name.strip() and product_description.strip():                                # .strip() é usado para tirar os espaços em branco.
-            break                                                                               # break é utilizado para quebrar o loop.
+        product_name = input('Insira o nome do produto: ')
+        product_description = input('Insira a descrição do produto: ')
+        if product_name.strip() and product_description.strip():
+            break
         else:
             linhas()
             print('Erro! Você deve dar um nome e uma descrição ao produto a ser calculado, para isso você deve inserir pelo menos um caracter.')
@@ -35,67 +83,58 @@ def inserirDados():
     while True:
         try:
             linhas()
-            product_price = float(input('Insira o custo do produto: '))                         #CP
-            fixed_price = float(input('Insira o percentual do custo fixo: '))                   #CF
-            commision = float(input('Insira o percentual da comissão de vendas: '))             #CV
-            taxes = float(input('Insira o percentual de impostos: '))                           #IV
-            rentability = float(input('Insira o percentual de rentabilidade desejado: '))       #ML
+            product_price = float(input('Insira o custo do produto: '))
+            fixed_price = float(input('Insira o percentual do custo fixo: '))
+            commision = float(input('Insira o percentual da comissão de vendas: '))
+            taxes = float(input('Insira o percentual de impostos: '))
+            rentability = float(input('Insira o percentual de rentabilidade desejado: '))
             break
         except ValueError:
             linhas()
-            print('Erro! Você deve inserir o custo do produto usando ponto como virgula; As porcentagens só devem aceitar valores numéricos (sem "%")')
+            print('Erro! Você deve inserir o custo do produto usando ponto como vírgula; As porcentagens só devem aceitar valores numéricos (sem "%")')
 
-    sale_price = (product_price / (1 - (fixed_price + commision + taxes + rentability) / 100))      #Fórmula do preço de venda
-    receita_bruta = sale_price - product_price                                                      #Fórmula da receita bruta
-    fixed_price_reais = sale_price * (fixed_price/100)                                              #Fórmula para calcular o custo fixo em reais
-    commision_reais = sale_price * (commision/100)                                                  #Fórmula para calcular a comissão em reais
-    taxes_reais = sale_price * (taxes/100)                                                          #Fórmula para calcular impostos em reais
-    other_costs_reais = fixed_price_reais + commision_reais + taxes_reais                           #Fórmula para somar os custos em reais
-    other_costs = fixed_price + commision + taxes                                                   #Fórmula para somar os custos em porcentagem
-    rentability_reais = receita_bruta - other_costs_reais                                           #fórmula para calcular a rentabilidade em reais
-    product_price_percentage = (product_price/sale_price)*100                                       #Fórmula para calcular o valor do produto em percentagem
-    receita_bruta_percentage = (receita_bruta/sale_price)*100                                       #Fórmula para calcular a receita bruta em porcentagen
+    # Calculando valores derivados
+    sale_price = (product_price / (1 - (fixed_price + commision + taxes + rentability) / 100))
+    receita_bruta = sale_price - product_price
+    fixed_price_reais = sale_price * (fixed_price / 100)
+    commision_reais = sale_price * (commision / 100)
+    taxes_reais = sale_price * (taxes / 100)
+    rentability_reais = receita_bruta - (fixed_price_reais + commision_reais + taxes_reais)
+    product_price_percentage = (product_price / sale_price) * 100
 
-    tabela_valores = [
-        ['Descrição',          'Valores',                                       'Porcentagens'],
-        ['Preço de venda',     'R${:.2f}'.format(sale_price),                   '100.00%'],
-        ['Custo de Aquisição', 'R${:.2f}'.format(product_price),   '{:.2f}%'.format(product_price_percentage)],  
-        ['Receita bruta',      'R${:.2f}'.format(receita_bruta),                '{:.2f}%'.format(receita_bruta_percentage)], 
-        ['Custo Fixo',         'R${:.2f}'.format(fixed_price_reais),            '{:.2f}%'.format(fixed_price)],
-        ['Comissão de vendas', 'R${:.2f}'.format(commision_reais),              '{:.2f}%'.format(commision)],
-        ['Impostos',           'R${:.2f}'.format(taxes_reais),                  '{:.2f}%'.format(taxes)],
-        ['Outros custos',      'R${:.2f}'.format(other_costs_reais),            '{:.2f}%'.format(other_costs)],
-        ['Rentabilidade',      'R${:.2f}'.format(rentability_reais),            '{:.2f}%'.format(rentability)],
+    # Verificar se todos os valores estão presentes
+    valores = [
+        primary_key,
+        product_name,
+        product_description,
+        product_price,
+        product_price_percentage,
+        fixed_price_reais,
+        fixed_price,
+        commision_reais,
+        commision,
+        taxes_reais,
+        taxes,
+        rentability,
+        rentability_reais,
+        sale_price
     ]
 
-    if rentability > 20:
-        lucro = 'alto'
-    if rentability > 10 and rentability <= 20:
-        lucro = 'médio'
-    if rentability > 0 and rentability <= 10:
-        lucro = 'baixo'
-    if rentability == 0:
-        lucro = 'nulo'
-    if rentability < 0:
-        lucro = 'prejuízo'
+    print("Valores a serem inseridos:", valores)
 
-tabela_valores = []
+    comandoSQL("INSERT INTO dadosproduto VALUES ({}, '{}', '{}', {:.2f}, {:.2f}, {:.2f}, {:.2f}, {:.2f}, {:.2f}, {:.2f}, {:.2f}, {:.2f}, {:.2f}, {:.2f})".format(
+        *valores
+    ))
+# Esta função tem que ser completamente mudada.
 
 def verTabela():
-    global tabela_valores
-    if tabela_valores == []:
-        linhas()
-        print('Não há tabelas para mostrar')
-    else:
-        linhas()
-        for item in tabela_valores:
-            print(':',
-                item[0],' '*(18-len(item[0])) + ':',                                                    # (x - len(item[y])) é uma fórmula que calcula quantos espaços em branco devem ser printados    
-                item[1],' '*(7-len(item[1]))  + ':',                                                    # sendo 'x' o tamanho máximo da coluna, 'y' o index number da coluna da matriz a ser formatada
-                item[2],' '*(12-len(item[2])) + ':')                                                    # e o comando len() serve para receber o tamanho da string e usá-lo na fórmula.
+    comandoSQL('SELECT * FROM dadosproduto;', select= True)
+
 
 def apagarDados():
-    print('Não implementado')
+    linhas()
+    valor = int(input('Insira o Codigo do produto que deseja apagar: '))
+    comandoSQL('DELETE FROM dadosproduto WHERE codigo = {}'.format(valor))
 
 def opcaoEscolhida(opcoes):
     linhas()
@@ -133,36 +172,3 @@ def menu():
             break
 
 menu()
-
-# tabela_lucros = [
-#     ['Classificação', 'Lucro'],
-#     ['Alto', '>  20%'],
-#     ['Médio', '>  10% ; <= 20%'],
-#     ['Baixo', '>  0% ; <= 10%'],
-#     ['Equilíbrio', '=  0%'],
-#     ['Prejuízo', '<  0%'],
-# ]
-
-# for item in tabela_lucros:
-#     print(':',
-#           item[0],' '*(13-len(item[0])) + ':',
-#           item[1],' '*(15-len(item[1])) + ':'
-#           )
-
-# if rentability > 20:
-#     lucro = 'alto'
-# if rentability > 10 and rentability <= 20:
-#     lucro = 'médio'
-# if rentability > 0 and rentability <= 10:
-#     lucro = 'baixo'
-# if rentability == 0:
-#     lucro = 'nulo'
-# if rentability < 0:
-#     lucro = 'prejuízo'
-    
-# linhas() 
-
-# print('O lucro do produto {} é classificado como {}.'.format(product_name.capitalize(), lucro))
-
-# linhas()
-
